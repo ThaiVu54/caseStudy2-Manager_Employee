3cac7e7c78290d60952d06a625bb654f0de91afb
ThaiVu54 <thai01654369440@gmail.com>
2021-10-22 08:34:30 +0700

    check user

--- src/model/User.java
+++ src/model/User.java
@@ -1,10 +1,25 @@
 package model;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User implements Serializable {
+    List<User> users = new ArrayList<>();
     private String userName;
     private String passWord;
     private String role;
+
+    public void addUser(User user){
+        users.add(user);
+    }
+    public boolean checkUser(String userName, String passWord){
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUserName().equals(userName)){
+                return true;
+            }
+        }
+        return false;
+    }
 
     public String getRole() {
         return role;
