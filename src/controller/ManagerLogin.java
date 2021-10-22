02ab899e99951da736e8aeec87eda8e8973c9d2c
ThaiVu54 <thai01654369440@gmail.com>
2021-10-22 09:06:15 +0700

    chưa chay chay login

--- src/controller/ManagerLogin.java
+++ src/controller/ManagerLogin.java
@@ -11,15 +11,25 @@ public class ManagerLogin {
 
 
     public static  void writeFileLogin() throws IOException {
+        File file = new File("src/login.txt");
+        if (!file.exists()){
+            file.createNewFile();
+        }
         FileOutputStream fos = new FileOutputStream("src/login.txt");
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(users);
     }
 
     public static void readFileLogin() throws IOException, ClassNotFoundException {
+        File file = new File("src/login.txt");
+        if (!file.exists()){
+            file.createNewFile();
+        }
         FileInputStream fis = new FileInputStream("src/login.txt");
         ObjectInputStream ois = new ObjectInputStream(fis);
         users = (ArrayList<User>) ois.readObject();
+        ois.close();
+        fis.close();
     }
 
     public static void removeLogin(String nameLogin) throws IOException, ClassNotFoundException {
